bc56185c551b3574746875b747d26e146e109671
ebtesam25 <ebtesam@iut-dhaka.edu>
2021-11-29 21:38:12 -0500

    updated readings

--- dashboard/app.py
+++ dashboard/app.py
@@ -7,10 +7,10 @@ import pandas as pd
 
 mapbox_access_token = 'pk.eyJ1IjoiZWJ0ZXNhbWhhcXVlIiwiYSI6ImNrdzhlcWwyN2ZvdjMycHBnanIwOGpncGkifQ.6cEyYNwDA9NeT01C_OrEow'
 
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/Nuclear%20Waste%20Sites%20on%20American%20Campuses.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/ebtesam25/waterwaze/main/dashboard/extendedreadings.csv?token=AGHHPHPMVYLPFGDYGBPFGXDBV3EB2')
 site_lat = df.lat
 site_lon = df.lon
-locations_name = df.text
+locations_name = df.id
 
 data = [
     go.Scattermapbox(
@@ -58,19 +58,7 @@ layout = go.Layout(
 
 fig = dict(data=data, layout=layout)
 
-fig2 = go.Figure(go.Scattermapbox(
-    mode = "markers+text+lines",
-    lon = [-75, -80, -50], lat = [45, 20, -20],
-    marker = {'size': 20, 'symbol': ["bus", "harbor", "airport"]},
-    text = ["Bus", "Harbor", "airport"],textposition = "bottom right"))
 
-fig2.update_layout(
-    mapbox = {
-        'accesstoken': mapbox_access_token,
-        'style': "outdoors", 'zoom': 0.7},
-    showlegend = False)
 
-fig2.show()
 
 plotly.offline.plot(fig)
-plotly.offline.plot(fig2)
